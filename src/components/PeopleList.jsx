e97710c10e8168f347d486d6ccc5ca155ecbc4c1
Romain Tello <romain.tello@gmail.com>
2020-03-06 10:28:51 +0100

    Add Fontawesome icons

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
 const PeopleList = ({ people, fetchData }) =>
   <div>
-    <Button onClick={fetchData}>
-      Actualiser
+    <Button variant="outline-primary" onClick={fetchData}>
+      <FontAwesomeIcon icon={faSyncAlt} />
     </Button>
     <ul>
       {people.map( (person, index) =>
